6afb1151edbb155f94a8a1fb68d3c12ac6faee2f
antjimriq <antoniojimenezriquelme@outlook.es>
2021-04-05 00:25:10 +0200

    Second commit Semana santa development update, made the layout of the list in which the games you have chosen are shown in the main page of the app

---
+++ app/src/main/java/com/aeg7/gamelog/Game.kt
@@ -0,0 +1,4 @@
+package com.aeg7.gamelog
+
+class Game(val id:Long, val name:String, val plattform:String) {
+}
--- app/src/main/java/com/aeg7/gamelog/my_games_list.kt
+++ app/src/main/java/com/aeg7/gamelog/my_games_list.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
-class my_games_list : Fragment() {
+open class my_games_list : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_games_list, container, false)
+        val view= inflater.inflate(R.layout.fragment_my_games_list, container, false)
+        val recycler=view.findViewById<RecyclerView>(R.id.my_games_recycler)
+        recycler.layoutManager=LinearLayoutManager(requireActivity())
+        return view
     }
 }
